8b3cfc48a738abd4c9e64a4cca74616e104a54a1
anjianshi <anjianshi@gmail.com>
2017-08-14 08:51:32 +0800

    项目重命名为 simple-config-reader

--- python/setup.py
+++ python/setup.py
@@ -6,14 +6,14 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='hg-config-reader',
+    name='simple-config-reader',
     version='0.0.1',
-    url='https://github.com/anjianshi/hg-config-reader',
+    url='https://github.com/anjianshi/simple-config-reader',
     license='MIT',
     author='anjianshi',
     author_email='anjianshi@gmail.com',
-    description="一个用于 huoguan.net 下各项目的简单的配置文件读取工具",
-    packages=['hg_config_reader', "hg_config_reader.utils"],
+    description="一个简单的配置文件读取工具",
+    packages=['simple_config_reader', "simple_config_reader.utils"],
     install_requires=['PyYAML'],
     zip_safe=False,
     platforms='any',
