4bf60ccf41829e38c2fd7abf6c67311d14272301
anjianshi <anjianshi@gmail.com>
2018-04-12 15:40:14 +0800

    调整 Python 下的代码习惯，改为使用单引号

--- python/simple_config_reader/__init__.py
+++ python/simple_config_reader/__init__.py
@@ -1,7 +1,7 @@
 import yaml
 from .utils import load_yaml, flatten_dict, deep_set, recursive_object_dict
 
-__all__ = ["read_config", "batch_read_config", "ReadConfigError"]
+__all__ = ['read_config', 'batch_read_config', 'ReadConfigError']
 
 
 class ReadConfigError(Exception):
@@ -13,7 +13,7 @@ class DefaultsLoader(yaml.SafeLoader):
 
 
 class NeedValue(yaml.YAMLObject):
-    yaml_tag = "!need"
+    yaml_tag = '!need'
     yaml_loader = DefaultsLoader
 
     @classmethod
@@ -33,8 +33,8 @@ def read_config(defaults_path, custom_values_path=None):
 
     unknown_paths = set(flatten_custom_values) - set(flatten_defaults)
     if unknown_paths:
-        formatted_paths = ", ".join(".".join(path) for path in unknown_paths)
-        raise ReadConfigError("custom_values 中出现了 defaults 里未定义的配置项：" + formatted_paths)
+        formatted_paths = ', '.join('.'.join(path) for path in unknown_paths)
+        raise ReadConfigError('custom_values 中出现了 defaults 里未定义的配置项：' + formatted_paths)
 
     config = {}
     missed_paths = []
@@ -46,8 +46,8 @@ def read_config(defaults_path, custom_values_path=None):
         deep_set(config, path, value)
 
     if missed_paths:
-        formatted_paths = ", ".join(".".join(path) for path in missed_paths)
-        raise ReadConfigError("以下配置项未赋值：" + formatted_paths)
+        formatted_paths = ', '.join('.'.join(path) for path in missed_paths)
+        raise ReadConfigError('以下配置项未赋值：' + formatted_paths)
 
     return recursive_object_dict(config)
 
@@ -82,6 +82,6 @@ def _deep_merge(a, b, path):
             if isinstance(a[key], dict) and isinstance(value, dict):
                 _deep_merge(a[key], value, sub_path)
             else:
-                raise ReadConfigError("配置项冲突：" + '.'.join(sub_path))
+                raise ReadConfigError('配置项冲突：' + '.'.join(sub_path))
         else:
             a[key] = value
--- python/simple_config_reader/utils/__init__.py
+++ python/simple_config_reader/utils/__init__.py
@@ -2,17 +2,17 @@ import yaml
 
 
 def load_yaml(path, loader=yaml.SafeLoader):
-    """读取并解析 yaml 文件的内容"""
+    '''读取并解析 yaml 文件的内容'''
     with open(path) as f:
         return yaml.load(f, loader)
 
 
 def flatten_dict(d, _parent_path=()):
-    """
+    '''
     将多层级的 dict 抹平，变成单层级的 { path: value } 格式
 
-    {"a": {"b": 1, "c": 2}}  =>  { ("a", "b"): 1, ("a", "c"): 2 }
-    """
+    {'a': {'b': 1, 'c': 2}}  =>  { ('a', 'b'): 1, ('a', 'c'): 2 }
+    '''
     flat_dict = {}
     for key, value in d.items():
         path = (*_parent_path, key)
@@ -24,7 +24,7 @@ def flatten_dict(d, _parent_path=()):
 
 
 def deep_set(d, path, value):
-    """深入设置 dict 某个下级节点的值，并在目标或中间节点不存在时，将其创建出来"""
+    '''深入设置 dict 某个下级节点的值，并在目标或中间节点不存在时，将其创建出来'''
     *init, tail = path
     for key in init:
         if key not in d:
@@ -34,9 +34,9 @@ def deep_set(d, path, value):
 
 
 class ObjectDict(dict):
-    """Makes a dictionary behave like an object, with attribute-style access.
+    '''Makes a dictionary behave like an object, with attribute-style access.
     from tornado.util
-    """
+    '''
     def __getattr__(self, name):
         try:
             return self[name]
@@ -51,7 +51,7 @@ class ObjectDict(dict):
 
 
 def recursive_object_dict(orig_dict):
-    """将 dict 递归转换成 ObjectDict，即下级的所有 dict 也都转换成 ObjectDict）"""
+    '''将 dict 递归转换成 ObjectDict，即下级的所有 dict 也都转换成 ObjectDict）'''
     return ObjectDict({
         key: (recursive_object_dict(value) if isinstance(value, dict) else value)
         for key, value in orig_dict.items()
